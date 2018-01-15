98add2f909903776410f0b2cf8b46ce586ea5073
jenczuen <jenczuen@gmail.com>
2018-01-15 01:28:41 +0100

    added top menu for main page

---
+++ sections/front-page-wd-top-menu.php
@@ -0,0 +1,12 @@
+
+	<div class="wd-top-menu">
+		<ul>
+			<li><a href="#static-page-content">Instytut</a></li>
+			<li><a href="#wd_updates">Aktualności</a></li>
+			<li><a href="#wd_essays">Opracowania</a></li>
+			<li><a href="#wd_projects">Projekty</a></li>
+			<li><a href="#wd_patron">Patron</a></li>
+			<li><a href="#wd_contact">Kontakt</a></li>
+			<li><a href="#wd_team">Zespół</a></li>
+		</ul> 
+	</div>
