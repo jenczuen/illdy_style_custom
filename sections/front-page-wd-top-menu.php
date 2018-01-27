b92f79cb90d04ecac4bd567b8b34c5b0de46cc78
jenczuen <jenczuen@gmail.com>
2018-01-27 15:39:02 +0100

    visual fixes for top menu

--- sections/front-page-wd-top-menu.php
+++ sections/front-page-wd-top-menu.php
@@ -1,4 +1,5 @@
 
+<!--
 	<div class="wd-top-menu">
 		<ul>
 			<li><a href="#static-page-content">Instytut</a></li>
@@ -10,3 +11,19 @@
 			<li><a href="#wd_team">Zespół</a></li>
 		</ul> 
 	</div>
+-->
+
+
+	<div>
+		<ul class="wd-top-menu">
+			<li><a href="#wd_team">Zespół</a></li>
+			<li><a href="#wd_contact">Kontakt</a></li>
+			<li><a href="#wd_patron">Patron</a></li>
+			<li><a href="#wd_projects">Projekty</a></li>
+			<li><a href="#wd_essays">Opracowania</a></li>
+			<li><a href="#wd_updates">Aktualności</a></li>									
+			<li><a href="#static-page-content">Instytut</a></li>
+		</ul> 
+	</div>
+
+
